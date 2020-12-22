67b5fcb2009ba93c7557aad83096087581ff809a
Aviv Shitrit <aviv.shitrit@campus.technion.ac.il>
2020-12-23 00:48:50 +0200

    new file:   .ipynb_checkpoints/Part0_Intro-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Part1_Sequence-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Part2_VAE-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Part3_GAN-checkpoint.ipynb 	new file:   .ipynb_checkpoints/py-sbatch-checkpoint.sh 	modified:   Part0_Intro.ipynb 	modified:   Part1_Sequence.ipynb 	modified:   Part3_GAN.ipynb 	modified:   environment.yml 	new file:   hw3/.ipynb_checkpoints/charnn-checkpoint.py 	new file:   hw3/__pycache__/__init__.cpython-38.pyc 	new file:   hw3/__pycache__/charnn.cpython-38.pyc 	modified:   hw3/charnn.py 	modified:   jupyter-lab.sh

--- jupyter-lab.sh
+++ jupyter-lab.sh
@@ -19,12 +19,12 @@
 # Conda parameters
 #
 CONDA_HOME=$HOME/miniconda3
-CONDA_ENV=cs236781-hw
+CONDA_ENV=cs236781-hw-gpu
 
 unset XDG_RUNTIME_DIR
 source $CONDA_HOME/etc/profile.d/conda.sh
 conda activate $CONDA_ENV
 
 # jupyter lab --no-browser --ip=$(hostname -I) --port-retries=100
-xvfb-run -a -s "-screen 0 1440x900x24" jupyter lab --no-browser --ip=$(hostname -I) --port-retries=100
+jupyter lab
 
